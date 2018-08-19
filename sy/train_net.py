58fae15b00b5797ed34460dc545852ef72ed0dc2
SuYuehua <yvette_suyu@163.com>
2018-08-19 17:00:51 +0800

    Update train_net.py

--- sy/train_net.py
+++ sy/train_net.py
@@ -1,21 +1,7 @@
 #!/usr/bin/env python
-# coding=utf-8
-# Copyright 2018 challenger.ai
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
 
 """
-Baseline codes for zero-shot learning task.
 This python script is train a deep feature extractor (CNN).
 The command is:     python train_CNN.py Animals True 0.05
 The first parameter is the super-class.
